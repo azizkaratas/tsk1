bb893e02d0e4143abf013ff41c40b29071f12e7c
azizkaratas <azimvaliyev21gmail.com.email>
2025-03-23 08:22:06 +0400

    tsk1

---
+++ front/a.js
@@ -0,0 +1,47 @@
+// class user {
+//     constructor(parameters) {
+//         parameters.ad = "Elcin";
+//         parameters.soyad = "mehemmedov";
+//         parameters.yas = 20;    }
+//         static check() {
+//             return 'men User clasinin funksiyasiyam';
+//         }
+//         yashesablayan() {
+//             let indi = new Date()
+//             let insantevelludu = new Date(this.yas)
+//             let yas1 = Math.floor(indi.getTime() = insantevelludu.getTime())
+//         }
+// }
+
+// function click1() {
+//     let random = Math.floor(Math.random()*19+90)
+// }
+// function click2() {
+//     let ran1 = Math.floor(Math.random()*70+150)
+// }
+// function click3() {
+//     console.log("bu butona kliklediniz");
+// }
+
+// class insan {
+//     constructor(parameters) {
+//         parameters.ad = "Ayxi";
+//         parameters.soyad = "Ismayilzade";
+//         parameters.yas = 21;
+//         parameters.ekran = ad,soyad;
+//     }
+//     function name(1) {
+//         let obj = {
+//             ad1: "Nergiz",
+//             soyad1: "Hesenli",
+//             yas1: 23,
+//         }
+//         let ekran = obj.ad1 obj.soyad1
+//         yasgoster() {
+//             console.log(obj.yas1);
+//         }
+//         yasartir() {
+//             let yasatri = Math.floor
+//         }
+//     }
+// }
